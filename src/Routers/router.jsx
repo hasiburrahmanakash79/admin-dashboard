17325eed5206e18849bd298c847fe9cdf78195b1
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-04-29 13:18:25 +0600

    crreate dashboard layout and design the deshboard

--- src/Routers/router.jsx
+++ src/Routers/router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
-import Main from "../Layouts/Main";
+import Dashboard from "../Pages/Dashboard/Dashboard";
+// import Main from "../Layouts/Main";
 
 const router = createBrowserRouter([
+    // {
+    //   path: "/",
+    //   element: <Main/>,
+    // },
     {
-      path: "/",
-      element: <Main/>,
-    },
+      path: '/',
+      element:<Dashboard/>
+    }
   ]);
 
 export default router;
